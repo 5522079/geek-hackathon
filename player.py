f055ac15c747b9c779c924b614da851aa0a35665
ibukani <ibukani777@gmail.com>
2024-06-22 14:25:38 +0900

    読み込む画像を追加

--- player.py
+++ player.py
@@ -1,13 +1,22 @@
 import pygame
 
+SCALE = 1.5
+
 class Player:
   def __init__(self, x, y):
     self.x = x
     self.y = y
-    self.img = pygame.image.load('sprites/yellowbird-midflap.png')
+
+    # 画像の読み込み
+    self.midimg = pygame.image.load('sprites/yellowbird-midflap.png')
+    self.midimg = pygame.transform.rotozoom(self.midimg, 0, SCALE)
+    self.downimg = pygame.image.load('sprites/yellowbird-downflap.png')
+    self.downimg = pygame.transform.rotozoom(self.downimg, 0, SCALE)
+    self.upimg = pygame.image.load('sprites/yellowbird-upflap.png')
+    self.upimg = pygame.transform.rotozoom(self.upimg, 0, SCALE)
 
   def draw(self, screen):
-    screen.blit(self.img, (self.x, self.y))
+    screen.blit(self.midimg, (self.x, self.y))
 
   def gravity(self):
-    self.y += 1
+    self.y += 8
