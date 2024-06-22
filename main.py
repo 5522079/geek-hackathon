e4db7ab21f64198400cedfc18f9681ec9e584d60
ibukani <ibukani777@gmail.com>
2024-06-22 14:55:36 +0900

    プレイヤーのジャンプを追加

--- main.py
+++ main.py
@@ -17,20 +17,26 @@ class Game:
     fps = pygame.time.Clock()
 
     while self.running:
-      # 終了処理
-      for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-          self.running = False
-
       #メイン処理
       self.screen.fill((0, 0, 0))
 
       player1.gravity()
+
+      # イベント処理
+      for event in pygame.event.get():
+        # 終了イベント
+        if event.type == pygame.QUIT:
+          self.running = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+          player1.jump()
+
+      # 描画処理
       player1.draw(self.screen)
 
       # 画面更新
       pygame.display.update()
-      fps.tick(60)
+      fps.tick(45)
 
     # 終了処理
     pygame.quit()
--- player.py
+++ player.py
@@ -1,11 +1,14 @@
 import pygame
 
 SCALE = 1.5
+MOVE_SPEED = 7
 
 class Player:
   def __init__(self, x, y):
     self.x = x
     self.y = y
+    self.jumpFlameCount = 0
+    self.flayingStopCount = 0
 
     # 画像の読み込み
     self.midimg = pygame.image.load('sprites/yellowbird-midflap.png')
@@ -18,5 +21,30 @@ class Player:
   def draw(self, screen):
     screen.blit(self.midimg, (self.x, self.y))
 
+  def jump(self):
+    if self.jumpFlameCount <= 4:
+      self.jumpFlameCount = 20
+
   def gravity(self):
-    self.y += 8
+    # 下まで行き過ぎないように
+    if self.y > 800:
+      self.y = 800
+
+    # ジャンプと重力
+    if self.jumpFlameCount > 0:
+      # ジャンプ中
+      self.y -= MOVE_SPEED 
+      self.jumpFlameCount -= 1
+
+      # ジャンプ終了後に少し止まる
+      if self.jumpFlameCount == 0:
+        self.flayingStopCount = 4
+
+
+    elif self.flayingStopCount > 0:
+      # ジャンプ終了後の停止中
+      self.flayingStopCount -= 1
+      
+      # 重力
+    else:
+      self.y += MOVE_SPEED
