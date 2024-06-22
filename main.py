56b5994191e6462dbd98df8f4d7a6fd36c403313
ibukani <ibukani777@gmail.com>
2024-06-22 18:22:30 +0900

    変更の適応

--- main.py
+++ main.py
@@ -1,39 +1,32 @@
 import pygame
 import player
-
 import obstacle
-
+import background as bg
 
 # 定数
-WIDTH = 600
+WITH = 600
 HEIGHT = 800
 
 class Game:
-    # ゲームの初期化
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
-        self.running = True
-        self.background = Background()  # 追加: 背景の初期化
+  # ゲームの初期化
+  def __init__(self):
+    pygame.init()
+    self.screen = pygame.display.set_mode((WITH, HEIGHT))
+    self.running = True
 
-
-
-def run(self):
+  def run(self):
     playerBird = player.PlayerBird(100, 100)
     pipe = obstacle.Obstacle(600, -100)
+    background = bg.Background()
     fps = pygame.time.Clock()
 
-
-        while self.running:
-            # メイン処理
-            self.background.update()  # 追加: 背景の更新
-            self.screen.fill((0, 0, 0))
-
-
+    while self.running:
+      #メイン処理
+      self.screen.fill((0, 0, 0))
 
       playerBird.update()
       pipe.update()
-
+      background.update()
 
       if playerBird.collides_with(pipe):
         print('Game Over')
@@ -43,30 +36,22 @@ def run(self):
         # 終了イベント
         if event.type == pygame.QUIT:
           self.running = False
->
 
-            # イベント処理
-            for event in pygame.event.get():
-                # 終了イベント
-                if event.type == pygame.QUIT:
-                    self.running = False
-
-                if event.type == pygame.MOUSEBUTTONDOWN:
-                    playerBird.jump()
-
+        if event.type == pygame.MOUSEBUTTONDOWN:
+          playerBird.jump()
 
       # 描画処理
+      background.draw(self.screen)
       playerBird.draw(self.screen)
       pipe.draw(self.screen)
 
+      # 画面更新
+      pygame.display.update()
+      fps.tick(45)
 
-            # 画面更新
-            pygame.display.update()
-            fps.tick(45)
-
-        # 終了処理
-        pygame.quit()
+    # 終了処理
+    pygame.quit()
 
 if __name__ == '__main__':
-    game = Game()
-    game.run()
+  game = Game()
+  game.run()
--- obstacle.py
+++ obstacle.py
@@ -1,4 +1,36 @@
-class Obsttacle:
+import pygame
+from random import randint
+
+class Obstacle:
   def __init__(self, x, y):
-    self.x = x
-    self.y = y
+    # どかんが流れる速度
+    self.scroll_speed = -10
+    # 上のどかんと下のどかんの間
+    gap = 200
+
+    self.top_x = x
+    self.top_y = y
+    self.top = pygame.image.load("sprites/pipe-green.png").convert_alpha()
+    self.top = pygame.transform.rotate(self.top, 180)
+    self.top_rect = self.top.get_rect(topleft=(self.top_x, self.top_y))
+
+    self.bottom_x = x
+    self.bottom_y = self.top_y + self.top.get_height() + gap
+    self.bottom = pygame.image.load("sprites/pipe-green.png").convert_alpha()
+    self.bottom_rect = self.bottom.get_rect(topleft=(self.bottom_x, self.bottom_y))
+
+  def draw(self, screen):
+    screen.blit(self.top, self.top_rect.topleft)
+    screen.blit(self.bottom, self.bottom_rect.topleft)
+
+  def update(self):
+    self.top_rect.move_ip(self.scroll_speed, 0)
+    self.bottom_rect.move_ip(self.scroll_speed, 0)
+    self.top_x = self.top_rect.x
+    self.bottom_x = self.bottom_rect.x
+
+    if self.top_rect.right < 0 and self.bottom_rect.right < 0:
+        self.top_rect.left = 600
+        self.top_rect.top = randint(-100, 0)
+
+        self.bottom_rect.left = 600
