3a5a507d36376962d7b933d96f90e715385671fb
hama0209 <mano2020ceu@gmail.com>
2024-06-22 17:12:11 +0900

    main.py

--- main.py
+++ main.py
@@ -1,46 +1,50 @@
 import pygame
 import player
+from background import Background  # 追加
 
 # 定数
-WITH = 600
+WIDTH = 600
 HEIGHT = 800
 
 class Game:
-  # ゲームの初期化
-  def __init__(self):
-    pygame.init()
-    self.screen = pygame.display.set_mode((WITH, HEIGHT))
-    self.running = True
+    # ゲームの初期化
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.running = True
+        self.background = Background()  # 追加: 背景の初期化
 
-  def run(self):
-    playerBird = player.PlayerBird(100, 100)
-    fps = pygame.time.Clock()
+    def run(self):
+        playerBird = player.PlayerBird(100, 100)
+        fps = pygame.time.Clock()
 
-    while self.running:
-      #メイン処理
-      self.screen.fill((0, 0, 0))
+        while self.running:
+            # メイン処理
+            self.background.update()  # 追加: 背景の更新
+            self.screen.fill((0, 0, 0))
 
-      playerBird.update()
+            playerBird.update()
 
-      # イベント処理
-      for event in pygame.event.get():
-        # 終了イベント
-        if event.type == pygame.QUIT:
-          self.running = False
+            # イベント処理
+            for event in pygame.event.get():
+                # 終了イベント
+                if event.type == pygame.QUIT:
+                    self.running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-          playerBird.jump()
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    playerBird.jump()
 
-      # 描画処理
-      playerBird.draw(self.screen)
+            # 描画処理
+            self.background.draw(self.screen)  # 追加: 背景の描画
+            playerBird.draw(self.screen)
 
-      # 画面更新
-      pygame.display.update()
-      fps.tick(45)
+            # 画面更新
+            pygame.display.update()
+            fps.tick(45)
 
-    # 終了処理
-    pygame.quit()
+        # 終了処理
+        pygame.quit()
 
 if __name__ == '__main__':
-  game = Game()
-  game.run()
+    game = Game()
+    game.run()
