159ea085db0e6218fa5c1493fcc9eb83d01e9629
ibukani <ibukani777@gmail.com>
2024-06-22 14:29:44 +0900

    コメントの追加

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import pygame
 import player
 
+# 定数
+WITH = 600
+HEIGHT = 800
+
 class Game:
+  # ゲームの初期化
   def __init__(self):
     pygame.init()
-    self.screen = pygame.display.set_mode((800, 600))
+    self.screen = pygame.display.set_mode((WITH, HEIGHT))
     self.running = True
 
   def run(self):
@@ -12,16 +17,22 @@ class Game:
     fps = pygame.time.Clock()
 
     while self.running:
+      # 終了処理
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           self.running = False
 
+      #メイン処理
       self.screen.fill((0, 0, 0))
+
       player1.gravity()
       player1.draw(self.screen)
+
+      # 画面更新
       pygame.display.update()
       fps.tick(60)
 
+    # 終了処理
     pygame.quit()
 
 if __name__ == '__main__':
