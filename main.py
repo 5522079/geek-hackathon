1b347e367baca316b3cb499237372fa3bb6fba7b
ibukani <ibukani777@gmail.com>
2024-06-23 14:31:58 +0900

    bgmの追加

--- main.py
+++ main.py
@@ -114,12 +114,18 @@ class Game:
   
   # ゲームの実行
   def run(self):
+    pygame.mixer.init(frequency = 44100)    # 初期設定
+    pygame.mixer.music.load("sound\BGM.mp3")     # 音楽ファイルの読み込み
+    pygame.mixer.music.set_volume(0.07)        # 音量調整
+    pygame.mixer.music.play(1)              # 音楽の再生回数を指定
+
     while True:
       retry = self.main_loop()
       if not retry:
         break
       self.running = True
 
+    pygame.mixer.music.stop()   
     pygame.quit()
 
 if __name__ == '__main__':
